1f7282a62ab65b9d8bd66f25249e2b397e8b2c3f
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2021-04-14 17:25:35 +0200

    basic api serverless function

---
+++ functions/basic-api.js
@@ -0,0 +1,11 @@
+const items = require('../assets/data')
+
+exports.handler = async () => {
+  return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    statusCode: 200,
+    body: JSON.stringify(items),
+  }
+}
