b13da7921a3d9cee334eb8a1fcaa55742271dbc4
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2021-04-14 17:25:08 +0200

    add static fake data

---
+++ assets/data.js
@@ -0,0 +1,24 @@
+const items = [
+  {
+    id: 'recmg2a1ctaEJNZhu',
+    name: 'utopia sofa',
+    price: 39.95,
+  },
+  {
+    id: 'recvKMNR3YFw0bEt3',
+    name: 'entertainment center',
+    price: 29.98,
+  },
+  {
+    id: 'recxaXFy5IW539sgM',
+    name: 'albany sectional',
+    price: 10.99,
+  },
+  {
+    id: 'recyqtRglGNGtO4Q5',
+    name: 'leather sofa',
+    price: 9.99,
+  },
+]
+
+module.exports = items
